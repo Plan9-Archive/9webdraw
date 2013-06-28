72799c171b789a8d1dd2cb9ac7e8d79c5f6776e9
dhoskin <dhoskin@localhost>
2013-06-28 10:39:59 +0000

    Refactor /dev/draw/new handling into its own file.

--- web/js/draw/draw9p.js
+++ web/js/draw/draw9p.js
@@ -126,27 +126,6 @@ Draw9p.create = function(name, perm, mode){
 	throw("creation not implemented");
 }
 
-Draw9p.pad11 = function(x){
-	var buf = [];
-	var s = String(x);
-	var i;
-
-	//do{
-	//	buf[i] = Math.floor(x % 10);
-	//	i += 1;
-	//}while(x = Math.floor(x / 10));
-
-	for(i = 0; i < 11 - s.length; ++i){
-		buf[i] = " ".charCodeAt(0);
-	}
-
-	for(i; i < 11; ++i){
-		buf[i] = s.charCodeAt(i - (11 - s.length));
-	}
-
-	return buf;
-}
-
 Draw9p.read = function(fid, offset, count){
 	with(this.Qids){
 		if(fid.qid.path == QDRAWNEW){
@@ -159,25 +138,6 @@ Draw9p.read = function(fid, offset, count){
 			return [];
 		}
 	}
-}
-
-Draw9p.readdrawnew = function(conn){
-	var buf = [];
-
-	buf = buf.concat(this.pad11(conn)).concat(" ");
-	buf = buf.concat(this.pad11(0)).concat(" ");
-	buf = buf.concat(this.pad11("r8g8b8")).concat(" ");
-	buf = buf.concat(this.pad11(0)).concat(" ");
-	buf = buf.concat(this.pad11(0)).concat(" ");
-	buf = buf.concat(this.pad11(0)).concat(" ");
-	buf = buf.concat(this.pad11(640)).concat(" ");
-	buf = buf.concat(this.pad11(480)).concat(" ");
-	buf = buf.concat(this.pad11(0)).concat(" ");
-	buf = buf.concat(this.pad11(0)).concat(" ");
-	buf = buf.concat(this.pad11(640)).concat(" ");
-	buf = buf.concat(this.pad11(480)).concat(" ");
-
-	return buf;
 }
 
 Draw9p.dirent = function(qid, offset){
