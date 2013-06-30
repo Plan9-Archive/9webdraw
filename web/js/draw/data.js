f9723ed67d7a3a83aac0bc48a1b88863237e0d48
dhoskin <dhoskin@localhost>
2013-06-30 23:40:26 +0000

    Add B{G,P}{SHORT,LONG} functions.

--- web/js/draw/data.js
+++ web/js/draw/data.js
@@ -1,3 +1,22 @@
+var BGSHORT = function(p){
+	return (p[0]<<0) | (p[1]<<8);
+}
+var BGLONG = function(p){
+	return (p[0]<<0) | (p[1]<<8) | (p[2]<<16) | (p[3]<<24);
+}
+var BPSHORT = function(p, v){
+	p[0] = (v) & 0xFF;
+	p[1] = (v >> 8) & 0xFF;
+	return p;
+}
+var BPLONG = function(p, v){
+	p[0] = (v) & 0xFF;
+	p[1] = (v >> 8) & 0xFF;
+	p[2] = (v >>16) & 0xFF;
+	p[3] = (v >> 24) & 0xFF;
+	return p;
+}
+
 Draw9p.writedrawdata = function(connid, offset, data){
 	var conn = this.conns[connid];
 	if(conn == undefined){
