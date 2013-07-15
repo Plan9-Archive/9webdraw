d5c7df0d77bc2a8171334a7e7d51ff63352816f0
dhoskin <dhoskin@localhost>
2013-07-15 03:48:11 +0000

    We don't actually want to draw lines in Memdraw.line(); we'l delegate it to Memdraw.poly() in the manner of libmemdraw.

--- web/js/draw/memdraw.js
+++ web/js/draw/memdraw.js
@@ -1,24 +1,28 @@
+var icossin2 = function(dx, dy){
+	var theta = Math.atan2(dx, dy);
+	return {
+		cos: Math.cos(theta),
+		sin: Math.sin(theta),
+		theta: theta
+	}
+}
+
 Memdraw = {
+	/* XXX Implement line endings. */
 	line: function(dst, p0, p1, end0, end1, radius, src, sp, op){
-		dst.ctx.save();
-		dst.ctx.beginPath();
-		var dy = p1.y - p0.y;
-		var dx = p1.x - p0.x;
-		var angle = Math.atan(dy/dx);
-		var length = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy,2));
+		var angle = icossin2(p1.y - p0.y, p1.x - p0.x);
+		var dx = (angle.sin * (2 * radius + 1))/2;
+		var dy = (angle.cos * (2 * radius + 1))/2;
 
-		ctx.translate(p0.x, p0.y);
-		ctx.rotate(angle);
-		ctx.beginPath();
-		ctx.moveTo(0, radius/2);
-		ctx.lineTo(length, radius/2);
-		ctx.lineTo(length, -radius/2);
-		ctx.lineTo(0, -radius/2);
-		ctx.lineTo(0, radius/2);
-		//ctx.clip();
-		ctx.fill();
+		var q = {
+			/* 1/2 is cargo-cult from /sys/src/libmemdraw/line.c ; why? */
+			x: p0.x + 1/2 + angle.cos/2,
+			y: p0.y + 1/2 + angle.sin/2
+		}
 
-		dst.ctx.restore();
+		var points = [];
+		points.append({x: q.x-dx, y: q.y+dy});
+		points.append({x: q.x+dx, y: q.y-dy});
 		return;
 	},
 	Ops: {
