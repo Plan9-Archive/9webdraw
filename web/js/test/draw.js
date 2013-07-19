6ca7c620bd7cb262ab57f9db1e9ed2d2f9db4bd3
dhoskin <dhoskin@localhost>
2013-07-19 19:12:04 +0000

    Draw test case: draw to intermediate, larger image.

--- web/js/test/draw.js
+++ web/js/test/draw.js
@@ -1,10 +1,17 @@
 Testdraw = function(){
 	var root = Draw9p.RootImage();
 
-	var img = new Draw9p.Image(0, "r8g8b8", 1,
+	var src = new Draw9p.Image(0, "r8g8b8", 1,
 		{min: {x: 0, y: 0}, max: {x: 1, y: 1}},
 		{min: {x: 0, y: 0}, max: {x: 1, y: 1}},
 		0x00FF00FF);
+
+	var img = new Draw9p.Image(0, "r8g8b8", 0,
+		{min: {x: 0, y: 0}, max: {x: 100, y: 100}},
+		{min: {x: 0, y: 0}, max: {x: 100, y: 100}},
+		0xFF00FFFF);
+
+	Memdraw.line(img, {x: 45, y: 15}, {x: 0, y: 100}, 0, 0, 10, src, {x: 0, y: 0}, 0);
 
 	Memdraw.line(root, {x: 15, y: 15}, {x: 100, y: 100}, 0, 0, 10, img, {x: 0, y: 0}, 0);
 }
