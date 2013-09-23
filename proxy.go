6f4795971a683ca4829df26b6e1c35918183bfb4
dhoskin <dhoskin@localhost>
2013-09-23 02:35:41 +0000

    Go proxy: add ability to launch clients (via runweb, currently unimplemented) or retain old behaviour, depending on -srv flag.

--- proxy.go
+++ proxy.go
@@ -5,17 +5,26 @@ import (
 	"os/exec"
 	"io"
 	"net/http"
+	"flag"
 )
 
-func srv9p(ws *websocket.Conn){
+func mksrv9p(usesrv bool, args []string) func(*websocket.Conn){
+return func(ws *websocket.Conn){
 	defer ws.Close();
 	ws.PayloadType = websocket.BinaryFrame;
 
-	srvcat := exec.Command("srvcat", "webdraw");
+	var srvcat *exec.Cmd;
+	if(usesrv){
+		srvcat = exec.Command("srvcat", "webdraw");
+	}else if(len(args) > 0){
+		srvcat = exec.Command("runweb", args...);
+	}else{
+		srvcat = exec.Command("runweb", "games/catclock");
+	}
 	stdin, err := srvcat.StdinPipe();
 	stdout, err := srvcat.StdoutPipe();
 
-	defer stdin.Close()
+	defer stdin.Close();
 	defer stdout.Close();
 
 	err = srvcat.Start();
@@ -31,13 +40,17 @@ func srv9p(ws *websocket.Conn){
 
 	io.Copy(ws, stdout);
 }
+}
 
 func main(){
+	usesrv := flag.Bool("srv", false, "post a file descriptor in srv");
+	flag.Parse();
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 		http.ServeFile(w, r, "web/9wd.html");
 	});
 	http.Handle("/js/", http.FileServer(http.Dir("web")));
 	http.Handle("/css/", http.FileServer(http.Dir("web")));
-	http.Handle("/9p", websocket.Handler(srv9p));
+	http.Handle("/9p", websocket.Handler(mksrv9p(*usesrv, flag.Args())));
+		
 	http.ListenAndServe("", nil);
 }
